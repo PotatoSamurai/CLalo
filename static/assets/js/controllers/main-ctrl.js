b052f26aada78bbcfe10879bdc29e157aeec3530
Juan Venegas <jrvboesch@gmail.com>
2014-12-16 22:07:09 -0600

    grunt setup

--- static/assets/js/controllers/main-ctrl.js
+++
@@ -1,7 +0,0 @@
-define(['./module'], function (controllers) {
-	'use strict';
-
-	controllers.controller('main-ctrl', ['$scope', function ($scope ) {
-			$scope.title= "";
-	}]);
-});
---
+++ static/assets/js/prod/directives/layout.js
@@ -0,0 +1,1 @@
+define(["./module"],function(directives){"use strict";directives.directive("header",function(){return{restrict:"E",templateUrl:"layout/index.html"}})});
